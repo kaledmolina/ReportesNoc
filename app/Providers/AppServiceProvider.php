edb08cb929be7c8f869b4afad4953e637f77b9ab
kaledmolynft <kaledmolynft@gmail.com>
2025-11-27 21:25:23 +0000

    beta 4.1.1 https v3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <--- 1. AGREGA ESTO ARRIBA
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +19,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // 2. AGREGA ESTE BLOQUE
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
     }
 }
