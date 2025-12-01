086341540ae43cde65757eb9836a3a605802e348
kaledmolynft <kaledmolynft@gmail.com>
2025-12-01 14:10:13 +0000

    beta5.1 hhttps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,5 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            // DESCOMENTAR ESTO:
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
